3375943cbf5b2e8b6045deeb0698b6e31c8cee6f
famirza2 <113233971+famirza2@users.noreply.github.com>
2023-03-29 07:22:32 -0500

    Updated to include a functional "Back to top" arrow

--- js/index.js
+++ js/index.js
@@ -1,6 +1,40 @@
+$(document).ready(function() {
 
 $(".contactMe").on("click", function (){
     $('html, body').animate({
         scrollTop: $(".flex-container").offset().top
     }, 300);
 });
+
+    var offset = 650;
+    var duration = 300;
+    var fadeOutDuration = 400;
+    var fadeInDuration = 200;
+    var controlHTML = '<img src="images/Scroll-To-Top-Blue-Arrow.png"/>';
+  
+  
+    $('<div>', {
+      id: 'back-to-top',
+      html: controlHTML,
+      css: {
+        position: 'fixed',
+        bottom: '50px',
+        right: '1px',
+        cursor: 'pointer',
+        display: 'none'
+      },
+      click: function() {
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+      }
+    }).appendTo('body');
+  
+  
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+        $('#back-to-top').fadeIn(fadeInDuration);
+      } else {
+        $('#back-to-top').fadeOut(fadeOutDuration);
+      }
+    });
+  });
